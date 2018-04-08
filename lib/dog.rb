40df0dd92383b6e854ffe261d3d659cd06d7b4c2
Michael Boroski <mjboroski@gmail.com>
2018-04-08 22:37:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,8 @@ class Dog
     @name = name
   end
 
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
 
   def name
@@ -16,8 +16,8 @@ class Dog
     @breed = breed
   end
 
-  def breed=(new_breed)
-    @breed = new_breed
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
