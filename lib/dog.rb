fddf707cf14d60c07e43afc81b985c0dbdf1652a
Michael Boroski <mjboroski@gmail.com>
2018-04-08 22:26:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+
   def initialize(name)
     @name = name
   end
@@ -10,4 +11,16 @@ class Dog
   def name
     @name
   end
-end    
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
