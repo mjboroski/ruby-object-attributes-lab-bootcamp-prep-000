bf0470d42d25c3edf4075cde603c313a1b9efe1d
Michael Boroski <mjboroski@gmail.com>
2018-04-08 22:27:16 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  def initialize(job)
+    @job = job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+end
